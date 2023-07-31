07fddc6758142ce193d9f3eab39157c676432c1f
Gustavo Lopez Flores <gustavo130403@gmail.com>
2023-07-30 21:55:01 -0400

    generics

--- src/estruturadedados/noeencadeamento/Main.java
+++ src/estruturadedados/noeencadeamento/Main.java
@@ -2,6 +2,7 @@ package estruturadedados.noeencadeamento;
 
 public class Main {
     public static void main(String[] args) {
+        /* NORMAL */
         No no1 = new No("Conteudo No 1");
         No no2 = new No("Conteudo No 2");
 
@@ -30,6 +31,33 @@ public class Main {
         System.out.println(no1.getProximoNo().getProximoNo().getProximoNo());
         System.out.println(no1.getProximoNo().getProximoNo().getProximoNo().getProximoNo());
 
+        System.out.println("-------------");
 
+        /* COM GENERICS */ // tornar classe nó genérica para aceitar conteudo de qualquer tipo
+        NoComGenerics<String> noComGenerics1 = new NoComGenerics<String>("Conteudo Generico 1");
+
+        NoComGenerics<String> noComGenerics2 = new NoComGenerics<>("Conteudo Generico 2"); // Segundo <> não precisa mais especificar o tipo, pois o compilador já entende que esta instanciando nó de String
+        noComGenerics1.setProximoNo(noComGenerics2);
+
+        NoComGenerics<String> noComGenerics3 = new NoComGenerics<String>("Conteudo Generico 3");
+        noComGenerics2.setProximoNo(noComGenerics3);
+
+        NoComGenerics<String> noComGenerics4 = new NoComGenerics<>("Conteudo Generico 4");
+        noComGenerics3.setProximoNo(noComGenerics4);
+
+        // no1 -> no2 -> no3 -> no4 -> null
+
+        System.out.println(noComGenerics1);
+        System.out.println(noComGenerics2);
+        System.out.println(noComGenerics3);
+        System.out.println(noComGenerics4);
+
+        System.out.println("-------------");
+
+        System.out.println(noComGenerics1);
+        System.out.println(noComGenerics1.getProximoNo());
+        System.out.println(noComGenerics1.getProximoNo().getProximoNo());
+        System.out.println(noComGenerics1.getProximoNo().getProximoNo().getProximoNo());
+        System.out.println(noComGenerics1.getProximoNo().getProximoNo().getProximoNo().getProximoNo());
     }
 }
---
+++ src/estruturadedados/noeencadeamento/NoComGenerics.java
@@ -0,0 +1,102 @@
+package estruturadedados.noeencadeamento;
+
+public class NoComGenerics<T> {
+    private T conteudo;
+    private NoComGenerics<T> proximoNo;
+
+    public NoComGenerics(T conteudo){
+        this.proximoNo = null;
+        this.conteudo = conteudo;
+    }
+
+    public T getConteudo(){
+        return conteudo;
+    }    
+
+    public void setConteudo(T conteudo){
+        this.conteudo = conteudo;
+    }
+
+
+    public NoComGenerics<T> getProximoNo(){
+        return proximoNo;
+    }
+
+    public void setProximoNo(NoComGenerics<T> proximoNo){
+        this.proximoNo = proximoNo;
+    }
+
+    @Override
+    public String toString(){
+        return conteudo.toString();
+    }
+}
+
+/* GENERICS
+ * 
+ * Lista<String> minhaLista = new Lista<>();
+ * 
+ * public class Lista<T>{
+ *      private T t;
+ * }
+ * 
+ * Contexto Generics:
+ * - Evita Casting Excessivo
+ * - Evita Código Redundante
+ * - Encontrar erros em tempo de compilação pois na Lista de exemplo acima
+ *   especificamos que só aceita Strings com o recurso de Generics
+ * 
+ * Wildcards (Coringas) : -> Antes de explicar como funciona o Generics é preciso saber
+ * - Unknown Wildcards (Unbounded) -> Ilimitado
+ * - Unbounded Wildcard (Upper Bounded / Lower Bounded)
+ * 
+ * Unknown Wildcards:
+ * 
+ * public void imprimeLista(List<?> lista){
+ *      for(Object obj : lista){
+ *          System.out.println(obj);
+ *      }
+ * }
+ * 
+ * List<Aluno> minhaLista = new List<Aluno>(); // declarando uma lista do tipo aluno
+ * imprimeLista(minhaLista); // imprime desde que tenha o toString implementado senão veremos apenas a referencia
+ * 
+ * Unbounded Wildcard - Upper Bounded:
+ * 
+ * // "Um Coringa limitado por Cima"
+ * // Só é possível passar listas de pessoas e descendentes(herdeiros de pessoas)
+ * // Ex: Aluno extends Pessoa, ele irá aceitar
+ * 
+ * public void imprimeLista(List<? extends Pessoa> listaPessoas){
+ *      for(Pessoa p : listaPessoas){
+ *          System.out.println(p);
+ *      }
+ * }
+ * 
+ * List<Aluno> minhaLista = new List<Aluno>(); // declarando uma lista do tipo aluno
+ * imprimeLista(minhaLista); // imprime desde que tenha o toString implementado senão veremos apenas a referencia
+ *  
+ * Unbounded Wildcard - Lower Bounded:
+ * 
+ * // "Um Coringa limitado por Baixo"
+ * // Qualquer lista de herdeiro de Pessoa o método ele não vai aceitar
+ * // Ex: Pessoa extends Cidadao, classe que pessoa herda é aceita
+ * 
+ * public void imprimeLista(List<? super Pessoa> listaPessoas){
+ *      for(Pessoa p : listaPessoas){
+ *          System.out.println(p);
+ *      }
+ * }
+ * 
+ * List<Aluno> minhaLista = new List<Aluno>(); // declarando uma lista do tipo aluno
+ * imprimeLista(minhaLista); // imprime desde que tenha o toString implementado senão veremos apenas a referencia
+ * 
+ * Convenção de Caracteres de Wildcards:
+ * 
+ * K - para "Key", exemplo Map<K,V>
+ * V - para "Value", exemplo Map<K,V>
+ * E - para "Element", exemplo List<E>
+ * T - para "Type", exemplo Collections#addAll
+ * ? - quando genérico
+ * 
+ */
