3cd845dece3051ede2dfff948853f88cae749594
Gustavo Lopez Flores <gustavo130403@gmail.com>
2023-10-02 22:28:30 -0400

    excecao encadeada com throws

--- src/excecoes/Main.java
+++ src/excecoes/Main.java
@@ -3,13 +3,26 @@ package excecoes;
 public class Main {
     public static void main(String[] args){
         try{
-            System.out.println(quociente(0, 3));
+            // System.out.println(quociente(0, 3));
+            metodoUm();
         }catch(ArithmeticException e){
             System.out.println("Entrou no catch...");
             System.out.println(e);
+        }catch (Exception e){
+            System.out.println("Entrou na excecao encadeada");
         }
     }
     public static int quociente(int divisor, int dividendo){
         return dividendo / divisor;
+    }
+    public static void metodoUm() throws Exception {
+        try{
+            metodoDois();
+        }catch(Exception e){
+            throw new Exception("Excecao no Primeiro Metodo", e);
+        }
+    }
+    public static void metodoDois() throws Exception{
+        throw new Exception("Excecao no Segundo Metodo");
     }
 }
