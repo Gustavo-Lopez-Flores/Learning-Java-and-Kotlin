4847c55381720b9e84e1a034c22b284e5d5df30a
Gustavo Lopez Flores <gustavo130403@gmail.com>
2023-10-02 22:55:36 -0400

    criando minha propria excecao

--- src/excecoes/Main.java
+++ src/excecoes/Main.java
@@ -1,15 +1,24 @@
 package excecoes;
 
+import excecoes.minhasexcecoes.ExcecaoContemVogal;
+
 public class Main {
     public static void main(String[] args){
         try{
             // System.out.println(quociente(0, 3));
-            metodoUm();
+            // metodoUm();
+            contemVogal("ABC");
         }catch(ArithmeticException e){
-            System.out.println("Entrou no catch...");
+            System.out.println("Entrou no catch de ArithmeticException");
+            System.out.println(e);
+        }catch(ExcecaoContemVogal e){
+            System.out.println("Entrou no catch de ExcecaoContemVogal");
             System.out.println(e);
         }catch (Exception e){
-            System.out.println("Entrou na excecao encadeada");
+            System.out.println("Entrou no catch de Exception");
+            System.out.println(e);
+        }finally {
+            System.out.println("fim...");
         }
     }
     public static int quociente(int divisor, int dividendo){
@@ -19,10 +28,15 @@ public class Main {
         try{
             metodoDois();
         }catch(Exception e){
-            throw new Exception("Excecao no Primeiro Metodo", e);
+            throw new Exception();
         }
     }
     public static void metodoDois() throws Exception{
-        throw new Exception("Excecao no Segundo Metodo");
+        throw new Exception();
+    }
+    public static void contemVogal(String texto) throws Exception{
+        if(texto.matches(".*[aeiouAEIOU].*")){
+            throw new ExcecaoContemVogal("O texto " + texto + " contem vogal");
+        }
     }
 }
---
+++ src/excecoes/minhasexcecoes/ExcecaoContemVogal.java
@@ -0,0 +1,7 @@
+package excecoes.minhasexcecoes;
+
+public class ExcecaoContemVogal extends RuntimeException{
+    public ExcecaoContemVogal(String message){
+        super(message);
+    }
+}
