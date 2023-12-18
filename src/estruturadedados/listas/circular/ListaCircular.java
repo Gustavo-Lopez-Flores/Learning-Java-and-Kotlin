225a09fc90c9e1dc4928e092d45d179bd62432ae
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2023-12-17 22:06:22 -0400

    Lista Circular - Métodos get() e getNo()

--- src/estruturadedados/listas/circular/ListaCircular.java
+++ src/estruturadedados/listas/circular/ListaCircular.java
@@ -1,10 +1,30 @@
 package estruturadedados.listas.circular;
 
-public class ListaCircular<T>{
-    
+public class ListaCircular<T>{    
     private No<T> cabeca;
     private No<T> cauda;
     private int tamanhoLista;
+
+    public T get(int index){
+        return this.getNo(index).getConteudo();
+    }
+
+    private No<T> getNo(int index){
+        if(isEmpty()){
+            throw new IndexOutOfBoundsException("A lista esta vazia!");
+        }
+
+        if(index == 0){
+            return this.cauda;
+        }
+
+        No<T> noAuxiliar = this.cauda;
+        for(int i = 0; (i < index) && (noAuxiliar != null); i++){
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+
+        return noAuxiliar;
+    }
 
     public boolean isEmpty(){
         return this.tamanhoLista == 0 ? true : false;
